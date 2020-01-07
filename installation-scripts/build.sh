c86c47c08b50cabf5cf70f2c3a40496cbdab8b50
Vicck-tech <59371212+Vicck-tech@users.noreply.github.com>
2020-01-07 09:35:37 -0600

    Update build.sh

--- installation-scripts/build.sh
+++ installation-scripts/build.sh
@@ -11,52 +11,17 @@
 buildFolder="$HOME/Viccktory-build"
 outFolder="$HOME/Viccktory-Out"
 
-#Setting variables
-#Let us change the name"
-#First letter of desktop small
-
-desktop="gnome"
-
-#build.sh
-oldname1="iso_name=arcolinux"
-newname1="iso_name=arcolinuxb-$desktop"
-
-oldname2='iso_label="arcolinux'
-newname2='iso_label="arcolinuxb-'$desktop
-
-#os-release
-oldname3='NAME="ArcoLinux"'
-newname3='NAME=ArcoLinuxB-'$desktop
-
-oldname4='ID=ArcoLinux'
-newname4='ID=ArcoLinuxB-'$desktop
-
-#lsb-release
-oldname5='DISTRIB_ID=ArcoLinux'
-newname5='DISTRIB_ID=ArcoLinuxB-'$desktop
-
-oldname6='DISTRIB_DESCRIPTION="ArcoLinux"'
-newname6='DISTRIB_DESCRIPTION=ArcoLinuxB-'$desktop
-
-#hostname
-oldname7='ArcoLinux'
-newname7='ArcoLinuxB-'$desktop
-
-#hosts
-oldname8='ArcoLinux'
-newname8='ArcoLinuxB-'$desktop
-
 echo
 echo "################################################################## "
 tput setaf 2;echo "Phase 1 : clean up and download the latest ArcoLinux-iso from github";tput sgr0
 echo "################################################################## "
 echo
 echo "Deleting the work folder if one exists"
-[ -d ../work ] && rm -rf ../work
+[ -d ../work ] && rm -rf work
 echo "Deleting the build folder if one exists - takes some time"
 [ -d $buildFolder ] && sudo rm -rf $buildFolder
 echo "Git cloning files and folder to work folder"
-git clone https://github.com/Vicck-tech/viccktory-iso ../work
+git clone https://github.com/Vicck-tech/viccktory-iso work
 
 echo
 echo "################################################################## "
@@ -64,32 +29,9 @@ tput setaf 2;echo "Phase 2 : Getting the latest versions for some important file
 echo "################################################################## "
 echo
 echo "Removing the old packages.x86_64 file from work folder"
-rm ../work/archiso/packages.x86_64
+rm work/archiso/packages.x86_64
 echo "Copying the new packages.x86_64 file"
-cp -f ../archiso/packages.x86_64 ../work/archiso/packages.x86_64
-
-echo "Removing old files/folders from /etc/skel"
-rm -rf ../work/archiso/airootfs/etc/skel/.* 2> /dev/null
-
-echo "getting .bashrc from arcolinux-root"
-wget https://raw.githubusercontent.com/arcolinux/arcolinux-root/master/etc/skel/.bashrc-latest -O ../work/archiso/airootfs/etc/skel/.bashrc
-
-echo
-echo "################################################################## "
-tput setaf 2;echo "Phase 3 : Renaming the ArcoLinux iso";tput sgr0
-echo "################################################################## "
-echo
-echo "Renaming to "$newname1
-echo "Renaming to "$newname2
-echo
-sed -i 's/'$oldname1'/'$newname1'/g' ../work/archiso/build.sh
-sed -i 's/'$oldname2'/'$newname2'/g' ../work/archiso/build.sh
-sed -i 's/'$oldname3'/'$newname3'/g' ../work/archiso/airootfs/etc/os-release
-sed -i 's/'$oldname4'/'$newname4'/g' ../work/archiso/airootfs/etc/os-release
-sed -i 's/'$oldname5'/'$newname5'/g' ../work/archiso/airootfs/etc/lsb-release
-sed -i 's/'$oldname6'/'$newname6'/g' ../work/archiso/airootfs/etc/lsb-release
-sed -i 's/'$oldname7'/'$newname7'/g' ../work/archiso/airootfs/etc/hostname
-sed -i 's/'$oldname8'/'$newname8'/g' ../work/archiso/airootfs/etc/hosts
+cp -f archiso/packages.x86_64 work/archiso/packages.x86_64
 
 echo
 echo "################################################################## "
@@ -152,11 +94,11 @@ echo
 
 echo "Copying files and folder to build folder as root"
 sudo mkdir $buildFolder
-sudo cp -r ../work/* $buildFolder
+sudo cp -r work/* $buildFolder
 
-sudo chmod 750 ~/arcolinuxb-build/archiso/airootfs/etc/sudoers.d
-sudo chmod 750 ~/arcolinuxb-build/archiso/airootfs/etc/polkit-1/rules.d
-sudo chgrp polkitd ~/arcolinuxb-build/archiso/airootfs/etc/polkit-1/rules.d
+sudo chmod 750 ~/ArcoViccktory-build/archiso/airootfs/etc/sudoers.d
+sudo chmod 750 ~/ArcoViccktory-build/archiso/airootfs/etc/polkit-1/rules.d
+sudo chgrp polkitd ~/ArcoViccktory-build/archiso/airootfs/etc/polkit-1/rules.d
 
 echo "Deleting the work folder if one exists - clean up"
 [ -d ../work ] && rm -rf ../work
@@ -187,7 +129,7 @@ echo "################################################################## "
 echo
 
 [ -d $outFolder ] || mkdir $outFolder
-cp $buildFolder/archiso/out/arcolinuxb* $outFolder
+cp $buildFolder/archiso/out/arcolinux* $outFolder
 
 echo
 echo "################################################################## "
