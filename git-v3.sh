eec3ce2fb35c539853abeaa702de7b95babb948b
Vicck-tech <59371212+Vicck-tech@users.noreply.github.com>
2020-01-08 11:47:52 -0600

    Update git-v3.sh

--- git-v3.sh
+++ git-v3.sh
@@ -4,8 +4,8 @@
 echo "Checking for newer files online first"
 git pull
 
-echo "Deleting the work folder if one exists"
-[ -d work ] && rm -rf work
+#echo "Deleting the work folder if one exists"
+#[ -d work ] && rm -rf work
 
 # Below command will backup everything inside the project folder
 git add --all .
